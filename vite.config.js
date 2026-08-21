fdc0708618938a321eb0c586c465890f18e305d3
divyanshukr456 <skylife4098@gmail.com>
2026-08-21 19:20:56 +0530

    Updated feature

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative assets work on GitHub Pages, Vercel, Netlify, and custom domains
+  base: '/AgriOS/',
 })
